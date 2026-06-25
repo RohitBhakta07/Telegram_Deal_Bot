1791389b3194e5485d695f19c6042729e6c370bd
Rohitbhakta07 <rohitbhakta324@gmail.com>
2026-06-26 01:12:42 +0530

    add files

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,7 +7,7 @@ import threading
 
 # Database file path setup
 current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
+parent_dir = os.path.dirname(current_dir) 
 sys.path.append(parent_dir)
 
 from database import db_manager
